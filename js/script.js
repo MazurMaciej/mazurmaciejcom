67c29d502c3bd749275c6e56aa1c43446176a5f7
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2020-04-28 14:25:12 +0200

    .

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@ const layer4 = document.querySelector('.layer-4');
 const layer5 = document.querySelector('.layer-5');
 const layer6 = document.querySelector('.layer-6');
 const layer7 = document.querySelector('.layer-7');
-const layer8 = document.querySelector('.layer-8');
+
 
 window.addEventListener('scroll', () => {
     let scrollPosition = window.pageYOffset;
     if (scrollPosition <= 550) {
-        console.log(scrollPosition)
         layer1.style.transform = `translateY(${scrollPosition * .99}px)`
         layer2.style.transform = `translateY(${scrollPosition * .9}px)`
         layer3.style.transform = `translateY(${scrollPosition * .7}px)`
@@ -18,7 +17,6 @@ window.addEventListener('scroll', () => {
         layer5.style.transform = `translateY(${scrollPosition * .15}px)`
         layer6.style.transform = `translateY(${scrollPosition * .05}px)`
         layer7.style.transform = `translateY(${scrollPosition * -.15}px)`
-        layer8.style.transform = `translateY(${scrollPosition * -.1}px)`
     }
 })
 
